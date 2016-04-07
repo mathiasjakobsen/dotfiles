872728757ecf2e0391793fd4014fb5544b7c0d58
Mathias Jakobsen <mathiasjakobsen@me.com>
2016-04-07 21:37:29 +0200

    Apache -> Nginx

--- hostfiles/reset.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-rm /etc/hosts
-cp /Users/mathiasjakobsen/dotfiles/hostfiles/hosts /etc/
-
-rm /etc/apache2/extra/httpd-vhosts.conf
-cp /Users/mathiasjakobsen/dotfiles/hostfiles/httpd-vhosts.conf /etc/apache2/extra/
-
-echo "Hostfiles was successfully reset.."
